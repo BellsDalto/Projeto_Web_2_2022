e6f7a523d27a568f0a258af1ff2df6d3dde9769a
Ana-Marie <anamarie1907@gmail.com>
2022-04-26 22:25:43 -0300

    consulta a API funcionando

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
-async function getJson(query) {
-    await fetch(`https://api.artic.edu/api/v1/artworks/search?q=${query}&fields=id,title,image_id&limite=5`)
-        .then(response.json()).then(function(data) {
-            console.log(data);
-            inputArtworkData(data);
-        });
+  function getJson(query) {
+      fetch(`https://api.artic.edu/api/v1/artworks/search?q=${query}&fields=id,title,image_id&limite=5`)
+          .then((r) => r.json()).then(function({ data }) {
+              inputArtworkData(data);
+          })
+          // .then(response.json()).then(function(response) {
+          // console.log(response);
+          //  inputArtworkData(data);
+          //  });*/
+          // let response = await fetch(`https://api.artic.edu/api/v1/artworks/search?q=${query}&fields=id,title,image_id&limite=5`)
+          // console.log(await response.json());
 
-}
+  }
 
-
-
-
-function inputArtworkData(docs) {
-    var content = document.querySelector('.artWorks_contents');
-    let ul = createElement('ul');
-    let li = createElement('li');
-    let img = createElement('img');
-    li.innerHTML = docs.title;
-    img.src = "https://www.artic.edu/iiif/2/" + docs.image_id + "/full/843,/0/default.jpg";
-    li.appendChild(img);
-    ul.appendChild(li);
-    content.appendChild(ul);
-    /* var content = document.querySelector('.artWorks_contents');
-     for (var i = 0; i < docs.length; i++) {
-         let ul = createElement('ul');
-         let li = createElement('li');
-         let img = createElement('img');
-         li.innerHTML = docs[i].title;
-         img.src = "https://www.artic.edu/iiif/2/" + docs[i].image_id + "/full/843,/0/default.jpg";
-         li.appendChild(img);
-         ul.appendChild(li);
-         content.appendChild(ul);
-
-     }*/
+  // utilizar async await ou promisses, ao usar promisses utilisar o metodo then().
 
 
-}
 
 
-function main() {
-    var search = document.querySelector('.search');
-    var query = search.value;
-    search.addEventListener("change", getJson(query));
+  function inputArtworkData(docs) {
+      var content = document.querySelector('.artWorks_contents');
+      for (var i = 0; i < docs.length; i++) {
+          let ul = document.createElement('ul');
+          let li = document.createElement('li');
+          let img = document.createElement('img');
+          li.innerHTML = docs[i].title;
+          img.src = "https://www.artic.edu/iiif/2/" + docs[i].image_id + "/full/843,/0/default.jpg";
+          li.appendChild(img);
+          ul.appendChild(li);
+          content.appendChild(ul);
+      }
+  }
 
 
-}
-main();
+  function main() {
+      var search = document.querySelector('.search');
+      var query = search.value;
+      search.addEventListener("keypress", function(e) {
+          if (e.key === "Enter") {
+              getJson(query)
+
+          }
+      });
+
+
+  }
+  main();
